91b25f2568e245a8a6dd040e33fe39a56a7e9a3a
Kyoy <183598258@qq.com>
2018-03-19 22:01:43 +0800

    判断代码行

--- src/WordCount.java
+++ src/WordCount.java
@@ -43,6 +43,23 @@ public class WordCount {
 
     public int getCodeLine(){return codeLine;}
 
+    public static int kindOfLine(String line){ //-1 empty 1 code 0 note
+        String temp ="";
+        if(line.trim().length() == 0)
+            return -1;
+        if(!line.contains("//") && !line.contains("/*") && !line.contains("*/"))  //不含这些符号必是code
+            return 1;
+        if(line.contains("//"))                                                   // //之前的code
+            temp = line.split("//")[0];
+        if(line.contains("/*"))                                                   // /*之前的code
+            temp = line.split("/\\*")[0];
+        if(line.contains("*/"))                                                   // */之后的code
+            temp = line.split("\\*/")[line.split("\\*/").length - 1];
+        if(temp.matches(".*\\w+.*"))                                      // 含有字母或数字则该行是code
+            return 1;
+        return 0;
+    }
+
     public WordCount(File file) {
         BufferedReader bf;
         this.resourceFile = resourceFile;
@@ -52,12 +69,13 @@ public class WordCount {
             while((temp1 = bf.readLine()) != null) {
                 temp2 += temp1 + String.valueOf('\n');
                 line++;
-                if(temp1.contains("//") || temp1.contains("/*") || temp1.contains("*/"))
-                    noteLine++;
-                else if(temp1.trim().length() != 0)
+                int i = kindOfLine(temp1);
+                if(i == -1)
+                    emptyLine++;
+                else if(i == 1)
                     codeLine++;
                 else
-                    emptyLine++;
+                    noteLine++;
                 for (String val: temp1.split(" |,")){
                     wordNum += val.equals("") ? 0 : 1;
 //                    System.out.println(val.equals("") ? "[space]" : val);
@@ -71,6 +89,34 @@ public class WordCount {
         }
     }
 
+//    public WordCount(File file) {
+//        BufferedReader bf;
+//        this.resourceFile = resourceFile;
+//        try {
+//            bf = new BufferedReader(new FileReader(file));
+//            String temp1, temp2 = "";
+//            while((temp1 = bf.readLine()) != null) {
+//                temp2 += temp1 + String.valueOf('\n');
+//                line++;
+//                if(temp1.contains("//") || temp1.contains("/*") || temp1.contains("*/"))
+//                    noteLine++;
+//                else if(temp1.trim().length() == 0)
+//                    emptyLine++;
+//                else
+//                    codeLine++;
+//                for (String val: temp1.split(" |,")){
+//                    wordNum += val.equals("") ? 0 : 1;
+////                    System.out.println(val.equals("") ? "[space]" : val);
+//                }
+//            }
+//            buffer = temp2.toCharArray();
+//            bf.close();
+//            charNum = buffer.length;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
     public WordCount(File file, HashSet<String> stopList) {
         BufferedReader bf;
         this.resourceFile = resourceFile;
@@ -80,12 +126,13 @@ public class WordCount {
             while((temp1 = bf.readLine()) != null) {
                 temp2 += temp1 + String.valueOf('\n');
                 line++;
-                if(temp1.contains("//") || temp1.contains("/*") || temp1.contains("*/"))
-                    noteLine++;
-                else if(temp1.trim().length() != 0)
+                int i = kindOfLine(temp1);
+                if(i == -1)
+                    emptyLine++;
+                else if(i == 1)
                     codeLine++;
                 else
-                    emptyLine++;
+                    noteLine++;
                 for (String val: temp1.split(" |,")){
                     wordNum += val.equals("") || stopList.contains(val) ? 0 : 1;
 //                    System.out.println(val.equals("") ? "[space]" : val);
@@ -101,10 +148,10 @@ public class WordCount {
     }
 
 
-//    public static void main(String args[]){
-//        WordCount wc = new WordCount(new File("D:\\idea-java\\WordCount\\src\\res\\ttt.txt"));
-//        System.out.println(wc.getWordNum() + " " + wc.getBuffer().length + " " + wc.getLine() + " " + System.getProperty("user.dir"));
-//
-//    }
+    public static void main(String args[]){
+        WordCount wc = new WordCount(new File("D:\\idea-java\\WordCount\\src\\res\\ttt.txt"));
+        System.out.println(wc.getWordNum() + " " + wc.getBuffer().length + " "  + wc.getCodeLine() + " " + wc.getEmptyLine() + " " + System.getProperty("user.dir"));
+
+    }
 
 }
